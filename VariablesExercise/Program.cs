8dbf85883eba2bacdf3c3ba4f0cc439f55690336
Kaddijatou Hydara <kad67414@gmail.com>
2025-01-12 11:46:04 -0500

    added variables to make code

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace VariablesExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String myName = "kaddija";
+        int age = 25;
+        double avg = 4.5;
+        decimal bankaccount = 10000.00m;
+        char initial = 'k';
+        bool ismarried = true;
+
+        Console.WriteLine($"Name: {myName}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Avg. bankaccount: {avg}");
+        Console.WriteLine($"Initial: {initial}");
+        Console.WriteLine($"Ismarried: {ismarried}");
+        
+
+        {
+        }
+        {
+            
+        {
+        }
+    }
+
+
+}
+}
